4cbaa4606c7e95e0fec0417525798dd99b75d9b4
sgm856 <179048454+sgm856@users.noreply.github.com>
2025-05-21 05:53:08 -0600

    added functions

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const add = function () {
+
+}
+
+const subtract = function () {
+
+}
+
+const multiply = function () {
+
+}
+
+const divide = function () {
+
+}
+
+const square = function () {
+
+}
+
+const powerTo = function () {
+
+}
+
+const operate = function () {
+
+}
+
