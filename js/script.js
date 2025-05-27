7e66eaf317c3ca9172df6e04328692a11778e2b4
sgm856 <179048454+sgm856@users.noreply.github.com>
2025-05-27 06:12:46 -0600

    added some js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+const sanitize = function (input) {
+    let cleanInput = input.toString.replace(/[^0-9.-]/g, '');
+    let number = Number.parseFloat(cleanInput);
+    return number;
+}
+
 const sum = function (...args) {
     let total = 0;
     for (addend of args) {
@@ -6,20 +12,28 @@ const sum = function (...args) {
     return total;
 }
 
-const multiply = function () {
-
+const multiply = function (...args) {
+    if (args.length === 0) {
+        return 0;
+    }
+    let product = 1;
+    for (factor of args) {
+        product *= factor;
+    }
+    return product;
 }
 
-const divide = function () {
-
-}
-
-const square = function () {
-
-}
-
-const powerTo = function () {
-
+const divide = function (...args) {
+    if (args.length === 0) {
+        return -0;
+    }
+    let quotient = args[0];
+    let term = 1;
+    for (let i = 1; i < args.length; i++) {
+        term = args[i]
+        quotient /= term;
+    }
+    return quotient;
 }
 
 const operate = function () {
